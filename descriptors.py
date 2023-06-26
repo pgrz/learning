efeba72119656d85e3feb18330be15a0d0f85158
pgrz <grzegorski.piotr@gmail.com>
2023-06-26 13:49:09 +0200

    Descriptors

---
+++ descriptors.py
@@ -0,0 +1,32 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class DirectoryNameLength:
+    def __get__(self, obj, objtype=None):
+        return len(obj.name)
+
+class Directory:
+    def __init__(self, name: str):
+        self.name = name
+
+    name_length = DirectoryNameLength()
+
+
+class Person:
+    def __init__(self, first_name: str, last_name: str):
+        self._first_name = first_name
+        self._last_name = last_name
+
+    @property
+    def name(self):
+        return " ".join((self._first_name, self._last_name))
+
+    name_length = DirectoryNameLength()
+
+def run_with_descriptor():
+    directory = Directory(name="asdf")
+    logger.info("Directory %s has a name length of %d", directory, directory.name_length)
+    person = Person(first_name="Bill", last_name="Cosby")
+    logger.info("Person %s has a name length of %d", person, person.name_length)
--- runner.py
+++ runner.py
@@ -4,6 +4,7 @@ import logging
 import context_managers
 import decorators
 import default_value
+import descriptors
 import generators
 import parallel_processing
 import quirks
@@ -15,6 +16,7 @@ logger = logging.getLogger(__name__)
 funcs_to_run = (
     context_managers.using_context_manager,
     decorators.run_decorated,
+    descriptors.run_with_descriptor,
     default_value.run_add_value,
     generators.run_generator,
     parallel_processing.run_threading,
